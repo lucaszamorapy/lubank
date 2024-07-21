a48b7c491062eaf0c15d524ed62574a9500a858d
lucaszamorapy <lucaszamora.zulmira@gmail.com>
2024-07-20 21:27:43 -0300

    feat: create functions for api requistions

---
+++ frontend/src/functions.tsx
@@ -0,0 +1,30 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:8081/api";
+
+type UserDataProps = {
+  username: string;
+  email: string;
+  password: string | number;
+  role_id: number;
+};
+
+export const getUsers = async () => {
+  try {
+    const response = await axios.get(`${API_URL}/users`);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    throw error; // Re-lançar o erro para que ele possa ser tratado pelos chamadores
+  }
+};
+
+export const createUser = async (userData: UserDataProps) => {
+  try {
+    const response = await axios.post(`${API_URL}/api/user`, userData);
+    return response.data;
+  } catch (error) {
+    console.error("Error creating user:", error);
+    throw error; // Re-lançar o erro para que ele possa ser tratado pelos chamadores
+  }
+};
